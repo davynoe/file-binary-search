3ba8d6cf462cdbb4c7266355a2d7a9aaa3afb396
davynoe <erentanyas04@gmail.com>
2023-11-07 01:25:39 +0300

    create search function

--- main.c
+++ main.c
@@ -3,9 +3,10 @@
 #include <string.h>
 
 #define WORD_SIZE 6 // word length + 1 for the null terminator
+#define LINE_COUNT 70
 
 char* getNameFromLine(int line, FILE* file) {
-    char *name = (char*)malloc(WORD_SIZE * sizeof(char)); 
+    char* name = (char*)malloc(WORD_SIZE * sizeof(char)); 
     int offset = (line-1) * WORD_SIZE; 
 
     fseek(file, offset, SEEK_SET);
@@ -14,19 +15,46 @@ char* getNameFromLine(int line, FILE* file) {
     return name;
 }
 
-int main() {
-    FILE* file = fopen("names.txt", "r");
+int search(char* name, char* fileName) {
+    FILE* file = fopen(fileName, "r");
 
     if (file == NULL) {
         perror("Error opening file");
         return 1;
     }
 
-    char* name = getNameFromLine(92, file);
-    puts(name);
+    int low = 1;
+    int high = LINE_COUNT;
+    char* lowName = getNameFromLine(low, file);
+    char* highName = getNameFromLine(high, file);
 
-    free(name);
+    while(strcmp(lowName, highName) <= 0) {
+        int mid = low + (high-low)/2;
+        char* midName = getNameFromLine(mid, file);
+        if(strcmp(name, midName) == 0) {
+            free(midName);
+            free(lowName);
+            free(highName);
+            fclose(file);
+            return mid;
+        }
+        else if(strcmp(name, midName) < 0) {
+            high = mid-1;
+        }
+        else {
+            low = mid+1;
+        }
+        free(midName);
+    }
+
+    free(lowName);
+    free(highName);
     fclose(file);
+    return -1; 
+}
 
+int main() {
+    int result = search("Zanee", "names.txt");
+    printf("%d\n", result);
     return 0;
 }
